6e5f469f9192056fdeec5ba710a240f141cbee7d
jeayoung <jjy0843@gmail.com>
2025-03-03 18:06:40 +0900

    feat : PROJ-37 : 유저 게임 승패 집계, 게임 기록 저장 엔티티 추가

--- src/main/java/com/genius/genius/domain/user/domain/User.java
+++ src/main/java/com/genius/genius/domain/user/domain/User.java
@@ -1,8 +1,13 @@
 package com.genius.genius.domain.user.domain;
 
 import com.genius.genius.common.entity.BaseEntity;
+import com.genius.genius.domain.rank.entity.Rank;
+import com.genius.genius.domain.record.entity.GameRecord;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -23,10 +28,21 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private String password;
 
-    @Column(name = "is_deleted", nullable = false)
+    @Column(nullable = false, name = "is_deleted")
     private boolean isDeleted;
 
     @Column(nullable = false, name = "authority")
     @Enumerated(EnumType.STRING)
     private Authority authority;
+
+    @OneToOne(mappedBy = "user")
+    private Rank rank;
+
+    // User가 첫 번째 사용자로 참여한 Record 들
+    @OneToMany(mappedBy = "winner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<GameRecord> recordsAsUser1 = new ArrayList<>();
+
+    // User가 두 번째 사용자로 참여한 Record 들
+    @OneToMany(mappedBy = "loser", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<GameRecord> recordsAsUser2 = new ArrayList<>();
 }
