700be231e6f64aeb84bf4876ab27a7a1016e6ba5
jmboy713 <jimin713@naver.com>
2025-03-03 13:49:32 +0900

    feat : PROJ-5 : User 객체 생성

---
+++ src/main/java/com/genius/genius/domain/room/controller/RoomController.java
@@ -0,0 +1,11 @@
+package com.genius.genius.domain.room.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/room")
+//@Tag(name = "Room", description = "방 API")
+public class RoomController {
+
+}
--- src/main/java/com/genius/genius/domain/room/domain/Room.java
+++ src/main/java/com/genius/genius/domain/room/domain/Room.java
@@ -1,22 +1,34 @@
 package com.genius.genius.domain.room.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import com.genius.genius.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, name = "is_started")
+    private Boolean isStarted;
+
+    @Column(nullable = false)
+    private User user1;
+
+    @Column(nullable = false)
+    private User user2;
 
 
 }
