cd422fa2715f658e8cb855dab1243639e8a79398
Jeayoung <jjy0843@gmail.com>
2025-04-30 12:44:23 +0900

    test : PROJ-26 : websocket 설정

---
+++ src/main/java/com/genius/genius/common/config/webSocket/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.genius.genius.common.config.webSocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 클라이언트가 구독할 prefix: /topic (또는 /queue)
+        registry.enableSimpleBroker("/topic");
+        // 클라이언트가 메시지 보낼 prefix: /app
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // WebSocket handshake endpoint
+        registry.addEndpoint("/ws-endpoint")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+}
